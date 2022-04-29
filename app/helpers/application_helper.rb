d1700a15ce4e6d7f283b24dc82d327bab9d32444
Fabian M. Fernandez <a2237mf@aiit.ac.jp>
2022-04-29 11:59:56 +0900

    Title with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(extra_title = '')
+    base_title = "FMF's Sample App"
+    if extra_title.empty?
+      base_title
+    else
+      extra_title + " | " + base_title
+    end
+  end
 end
