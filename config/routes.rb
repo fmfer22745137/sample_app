aa744cd6326d037bfae5843449b06838cf5c8f52
Fabian M. Fernandez <a2237mf@aiit.ac.jp>
2022-05-06 01:55:12 +0900

    Finish ch05: Sign up test, routes and initial template

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root "static_pages#home"
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
   get '/login', to: 'static_pages#login'
+  get '/signup', to: 'users#new'
 end
