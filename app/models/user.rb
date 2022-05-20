e0483184533c6e7e5bbbe23dbe30e20418df75fd
Fabian M. Fernandez <a2237mf@aiit.ac.jp>
2022-05-20 20:39:34 +0900

    Flash and update

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
                       length: { maximum: 255 },
                       uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
   has_secure_password
 
   def User.digest(string)
