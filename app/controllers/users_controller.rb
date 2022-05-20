002fe5b5490761d6c50a0636cf7f8f750e3018eb
Fabian M. Fernandez <a2237mf@aiit.ac.jp>
2022-05-20 21:59:25 +0900

    destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
 
   def index
     @users = User.paginate(page: params[:page])
@@ -39,6 +41,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -46,6 +54,7 @@ class UsersController < ApplicationController
                                  :password_confirmation)
     end
 
+    # 認可
     def logged_in_user
       unless logged_in?
         store_location
@@ -54,8 +63,14 @@ class UsersController < ApplicationController
       end
     end
 
+    # 自分自身かどうか
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
