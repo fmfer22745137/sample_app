dbaa92d7f86ea3df99555041ba8591ea587fe7b0
Fabian M. Fernandez <a2237mf@aiit.ac.jp>
2022-06-03 16:30:59 +0900

    search functions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,15 @@ class StaticPagesController < ApplicationController
     end    
   end
 
+  def search
+    keyword = params[:keyword]
+    all_users = User.where("name LIKE ?", "%#{keyword}%")
+    @users = all_users.paginate(page: params[:page], per_page: 5)
+
+    all_microposts = Micropost.where("content LIKE ?", "%#{keyword}%")
+    @microposts = all_microposts.paginate(page: params[:page], per_page: 15)
+  end
+
   def help
   end
 
